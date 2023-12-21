14215f0178d76ab5324e60f48d06b14955104092
Peder Riis <pede0821@koldinghfogvuc.dk>
2023-12-21 14:43:28 +0100

    come on

---
+++ codewars/BusinessController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars
+{
+    public static class BusinessController
+    {
+
+        public static void Run()
+        {
+            CukooClock();
+        }
+       public static void CukooClock()
+        {
+           Console.WriteLine("final " + CuckooClockSolution.CuckooClock("10:00", 10));
+        }
+    }
+}
---
+++ codewars/CuckooClockSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars
+{
+    public class CuckooClockSolution
+    {
+        public static string CuckooClock(string inputTime, int chimes)
+        {
+            var parsedTime = DateTime.Parse(inputTime);
+
+
+            for (int i = chimes-1; i > 0; i--)
+            {
+                int currentMinuts = short.Parse(parsedTime.ToString("mm"));
+
+                if (currentMinuts % 15 == 0 && currentMinuts != 0)
+                {
+                    chimes--;
+                    Console.WriteLine(parsedTime.ToString("HH:mm"));
+                    Console.WriteLine("Current chimes " + chimes);
+
+                }
+                else if (currentMinuts == 0)
+                {
+                    chimes = chimes - short.Parse(parsedTime.ToString("HH"));
+                    Console.WriteLine(parsedTime.ToString("HH:mm"));
+                    Console.WriteLine("Current chimes " + chimes);
+                }
+                parsedTime = parsedTime.AddMinutes(1);
+
+            }
+            
+            return parsedTime.ToString("HH:mm");
+        }
+
+    }
+}
